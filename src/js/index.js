d50587bba5ed4e6556d574782ec34e5ac1001b68
Giusy Cammarano <giusy@Giusys-iMac.local>
2020-04-09 16:41:41 +0100

    building the search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,42 @@
+// https://forkify-api.herokuapp.com/api/search
+
+import Search from './modules/Search';
+
+/** global state of the app
+ *  -search object
+ * - current recipe object
+ * - shopping list object
+ * - liked recipes
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+    //1) get query from view
+    const query = 'pizza';
+
+    if(query){
+        //2) new search object and add to state
+        state.search = new Search(query);
+
+        //3)prepare UI for results
+
+        //4) search for recipes
+        await state.search.getResults();
+
+        //5) render result on UI
+        console.log(state.search.result)
+
+    }
+
+
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
+
 
 
 
