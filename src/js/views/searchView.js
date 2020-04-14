146d61032aaf8e64e02c806b112c924b7f3ad6c1
Giusy Cammarano <giusy@Giusys-iMac.local>
2020-04-14 12:29:58 +0100

    building search view part2

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -24,6 +24,30 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 };
 
+/* 'pasta with tomato and spinach
+acc:0 / acc+ curr.length  = 5 / newTitle = ['pasta]
+acc:5 / acc+ curr.length  = 9 / newTitle = ['pasta' , 'with]
+acc:9 / acc+ curr.length  = 15 / newTitle = ['pasta' , 'with', 'tomato']
+acc:15 / acc+ curr.length  = 18 / newTitle = ['pasta' , 'with', 'tomato']
+acc:18 / acc+ curr.length  = 24 / newTitle = ['pasta' , 'with', 'tomato']
+*/ 
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit){
+        title.split(' ').reduce((acc, cur) => {
+            if(acc + cur.length <=  limit){
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+    
+        }, 0);
+
+        //return the result
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+};
 const renderRecipe = recipe => {
     const markup = ` <li>
                         <a class="results__link" href="${recipe.recipe_id}">
@@ -31,7 +55,7 @@ const renderRecipe = recipe => {
                                 <img src="${recipe.image_url}" alt="${recipe.title}">
                             </figure>
                             <div class="results__data">
-                                <h4 class="results__name">${recipe.title}</h4>
+                                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                                 <p class="results__author">${recipe.publisher}</p>
                             </div>
                         </a>
