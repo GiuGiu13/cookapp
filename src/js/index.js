bce23d323cebe2a0984d7fd3fb778641bbdc0801
Giusy Cammarano <giusy@Giusys-iMac.local>
2020-04-14 11:38:40 +0100

    building search view part 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 // https://forkify-api.herokuapp.com/api/search
 
+import { elements } from './views/base';
 import Search from './modules/Search';
+import * as searchView from './views/searchView';
 
 /** global state of the app
  *  -search object
@@ -13,26 +15,29 @@ const state = {};
 
 const controlSearch = async () => {
     //1) get query from view
-    const query = 'pizza';
-
+    const query = searchView.getInput();
+   
     if(query){
         //2) new search object and add to state
         state.search = new Search(query);
 
         //3)prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
 
         //4) search for recipes
         await state.search.getResults();
 
         //5) render result on UI
         console.log(state.search.result)
+        searchView.renderResults(state.search.result);
 
     }
 
 
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 })
